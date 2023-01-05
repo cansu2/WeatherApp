ba8f7c0c9bf0ea9b57e27dc883fe394c128ac015
CansuSlalom <cansu.alaeddinoglu@slalom.com>
2023-01-05 15:47:58 -0800

    SF version

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Cansu Alaeddinoglu on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
